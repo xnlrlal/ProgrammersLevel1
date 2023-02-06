74d6ba6e51c3e77e2bb237faef398acb39836039
xnlrlal <rlatjdgh227@gmail.com>
2023-02-06 20:46:41 +0900

    문제 설명, 제한사항 추가

--- src/EvenAndOdd.java
+++ src/EvenAndOdd.java
@@ -1,3 +1,12 @@
+/*
+    -- 짝수와 홀수 --
+    문제 설명
+        정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수, solution을 완성해주세요.
+
+    제한 조건
+        - num은 int 범위의 정수입니다.
+        - 짝수입니다.
+ */
 public class EvenAndOdd {
     public String solution(int num) {
         String answer;
